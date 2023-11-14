50b07e04f4ed63dfd001bfc589f39bd839c8a46b
williams-bruce <williamsbruceprep@gmail.com>
2023-11-14 19:09:00 -0300

    alterações no cliente

--- serverapp/client/clientV2.py
+++ serverapp/client/clientV2.py
@@ -1,32 +1,54 @@
 import socket
 import os
 from threading import Thread
+import pathlib
+import time
+import shutil
 
 
-SERVER: str = '127.0.0.1'
+SERVER: str = '192.168.0.137'
 PORT: int = 5050
 ADDR: tuple[str, int]= (SERVER, PORT)
 FORMAT = 'utf-8'
 ESCAPE_TOKEN = '\n'
 TEMP_DIR = '.client'
+TEMP_FILE_COM = 'transfer.txt'
 
 
-def get_username()->str:
-    with open(os.path.join('.client','nickname.txt'), 'r') as f:
-        nickname = f.readline()
-        return nickname
+def get_nickname()->str:
+    try:
+        with open(os.path.join('.client','transfer.txt'), 'r') as f:
+            texto = f.read()
+            lista = texto.splitlines()
+            if lista[0] == '<NICKNAME>':
+                return lista[1]
+            else:
+                print('Problemas ao abrir arquivo transfer.txt')
+    except:
+        print('Problemas ao abrir arquivo nickname.')
 
 
 def get_points()->str:
-    with open(os.path.join('.client','transfer.txt'), 'r') as f:
-        for line in f:
-            if line.startswith('<POINTS>'):
-                s = line.split('/*')
-                return s[1]
+    try:
+        with open(os.path.join(TEMP_DIR,'transfer.txt'), 'r') as f:
+            texto = f.read()
+            lista = texto.splitlines()
+            if lista[0] == '<POINTS>':
+                return lista[1]
+            else:
+                print('Problemas ao abrir arquivo transfer.txt')
+    except:
+        print('Problemas ao abrir arquivo transfer.txt')
+
+
+def mkdir_client():
+    #Criar
+    if not (aux := pathlib.Path(TEMP_DIR)).exists():
+        aux.mkdir()
 
 
 def write_to_file(mensagem: str)->None:
-    with open(os.path.join('.client','transfer.txt'), 'a') as f:
+    with open(os.path.join('.client','transfer.txt'), 'w') as f:
         f.write(mensagem)
 
 
@@ -38,13 +60,24 @@ def main():
     except:
         return print('\nNão foi possível conectar ao servidor...')
 
-    print('Conectado...')
+    print(f'Conectado a {SERVER}...')
+    mkdir_client()
 
     thread1 = Thread(target=receive_message, args=[client])
     thread2 = Thread(target=send_message, args=[client])
+    
+    threads = [thread1, thread2]
 
     thread1.start()
     thread2.start()
+    
+    while True:
+        time.sleep(1)
+        if all(not thread.is_alive() for thread in threads):
+            print('Jogo encerrado. Fechando tudo...')
+            break
+    shutil.rmtree(pathlib.Path(TEMP_DIR))
+    
 
 
 def receive_message(client: socket.socket):
@@ -57,10 +90,21 @@ def receive_message(client: socket.socket):
     while True:
         try:
             msg = client.recv(1024).decode(FORMAT)
+            print(f'\nServidor -> Cliente: {msg}')
             write_to_file(msg)
+            # msgs = msg.split(ESCAPE_TOKEN)
+            # match(msgs[0]):
+            #     case '<CONFIG>':
+            #         write_to_file(msg)
+            #     case '<BEGIN>':
+            #         write_to_file(msg)
+            #     case '<RANK>':
+            #         write_to_file(msg)
+            #     case '<ENDGAME>':
+            #         client.send(f'<POINTS>{ESCAPE_TOKEN}{get_points()}'.encode(FORMAT))
         except:
             print('Não foi possível continuar conectado...')
-            print('Pressione <enter> para continuar...')
+            input('Pressione <enter> para continuar...')
             client.close()
             break
 
@@ -74,17 +118,27 @@ def send_message(client: socket.socket):
 
     while True:
         try:
-            msg = input('\n')
-            msgs = msg.split(ESCAPE_TOKEN)
-            match(msgs[0]):
-                case '<STOP>':
-                    client.send(f'{msgs[0]}{ESCAPE_TOKEN}'.encode(FORMAT))
-                case '<NEXT>':
-                    client.send(f'{msgs[0]}{ESCAPE_TOKEN}'.encode(FORMAT))
-                case '<POINTS>':
-                    client.send(f'{msgs[0]}{ESCAPE_TOKEN}{get_points()}'.encode(FORMAT))
-                case '<NICKNAME>':
-                    client.send(f'{msgs[0]}{ESCAPE_TOKEN}{get_username()}'.encode(FORMAT))
+            if client:
+                msg = ''
+                if (pathlib.Path(TEMP_DIR)/TEMP_FILE_COM).exists():
+                    with (pathlib.Path(TEMP_DIR)/TEMP_FILE_COM).open() as file:
+                        msg = file.read()
+                        msgs = msg.split(ESCAPE_TOKEN)
+                        match(msgs[0]):
+                            case '<STOP>':
+                                client.send(f'{msgs[0]}{ESCAPE_TOKEN}'.encode(FORMAT))
+                            case '<NEXT>':
+                                client.send(f'{msgs[0]}{ESCAPE_TOKEN}'.encode(FORMAT))
+                            case '<POINTS>':
+                                client.send(f'{msgs[0]}{ESCAPE_TOKEN}{get_points()}'.encode(FORMAT))
+                            case '<NICKNAME>':
+                                client.send(f'{msgs[0]}{ESCAPE_TOKEN}{get_nickname()}'.encode(FORMAT))
+                    (pathlib.Path(TEMP_DIR)/TEMP_FILE_COM).unlink()
+                else:
+                    continue
+            else:
+                client.close()
+                return
         except:
             client.close()
             return
@@ -92,3 +146,10 @@ def send_message(client: socket.socket):
 
 if __name__ == '__main__':
     main()
+    
+
+#<NICKNAME>
+#BRUCE_LEE
+
+#<POINTS>
+#125
